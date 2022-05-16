1ab393d92460c5b46544cecf90899bbba3ebb583
Tiana Coker <tianacoker1@gmail.com>
2022-05-16 17:40:54 +0100

    Display Summary on Dashboard

--- admin/index.php
+++ admin/index.php
@@ -18,27 +18,60 @@
 
 
      <div class="col-4 text-center">
-       <h1>5</h1>
+       <?php 
+       $sql = "SELECT * FROM tbl_category";
+       $res = mysqli_query($conn, $sql);
+       $count = mysqli_num_rows($res);
+       
+       
+       ?>
+       <h1><?php echo $count; ?></h1>
        <br />
        Categories
      </div>
 
      <div class="col-4 text-center">
-       <h1>5</h1>
+       <?php 
+       $sql2 = "SELECT * FROM tbl_food";
+       $res2 = mysqli_query($conn, $sql2);
+       $count2 = mysqli_num_rows($res2);
+       
+       
+       ?>
+       <h1><?php echo $count2; ?></h1>
        <br />
-       Categories
+       Foods
      </div>
 
      <div class="col-4 text-center">
-       <h1>5</h1>
+        <?php 
+          $sql3 = "SELECT * FROM tbl_order";
+          $res3 = mysqli_query($conn, $sql3);
+          $count3 = mysqli_num_rows($res3);
+          
+          
+          ?>
+       
+       <h1><?php echo $count3; ?></h1>
        <br />
-       Categories
+       Total Orders
      </div>
 
      <div class="col-4 text-center">
-       <h1>5</h1>
+       <?php 
+       //use aggregate function in sql to get total revenue
+       $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered'";
+       $res4 = mysqli_query($conn, $sql4);
+
+       //get the total revenue value
+
+       $row4 = mysqli_fetch_assoc($res4);
+       $total_revenue = $row4['Total'];
+       
+       ?>
+       <h1>$<?php echo $total_revenue; ?></h1>
        <br />
-       Categories
+       Total Revenue 
      </div>
      <div class="clearfix"></div>
    </div>
